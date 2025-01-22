199dbcfd2671362df5e95a38e9a0ec2b2cc07199
Michal Josef Špaček <tupinek@gmail.com>
2025-01-22 18:19:16 +0100

    Remove simple comments

--- Singapore.pm
+++ Singapore.pm
@@ -1,11 +1,9 @@
 package Map::Tube::Singapore;
 
-# Pragmas.
 use strict;
 use warnings;
 use 5.006;
 
-# Modules.
 use File::Share ':all';
 use Moo;
 use namespace::clean;
@@ -118,11 +116,9 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =head1 EXAMPLE1
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::Singapore;
 
  # Object.
@@ -139,11 +135,9 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =head1 EXAMPLE2
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::Singapore;
 
  # Object.
@@ -160,11 +154,9 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =head1 EXAMPLE3
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::GraphViz;
  use Map::Tube::GraphViz::Utils qw(node_color_without_label);
  use Map::Tube::Singapore;
@@ -197,11 +189,9 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =head1 EXAMPLE4
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(encode_utf8);
  use Map::Tube::Singapore;
 
@@ -223,11 +213,9 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =head1 EXAMPLE5
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(encode_utf8);
  use Map::Tube::Singapore;
 
