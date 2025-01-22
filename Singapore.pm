e753bde5ccd5e93af07087bbfdec020ce2cb2971
Michal Josef Špaček <tupinek@gmail.com>
2025-01-22 19:14:37 +0100

    Prepare for 0.06 version

--- Singapore.pm
+++ Singapore.pm
@@ -9,7 +9,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Get XML.
 has xml => (
@@ -325,6 +325,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
